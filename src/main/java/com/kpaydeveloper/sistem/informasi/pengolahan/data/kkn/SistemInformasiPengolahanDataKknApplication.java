9475889b56ec1a6e745732d4d41f7e57ed394b7b
Deni Gunawan <artdengun@gmail.com>
2020-11-04 21:20:58 +0700

    sistem informasi pengolahan data kkn

---
+++ src/main/java/com/kpaydeveloper/sistem/informasi/pengolahan/data/kkn/SistemInformasiPengolahanDataKknApplication.java
@@ -0,0 +1,13 @@
+package com.kpaydeveloper.sistem.informasi.pengolahan.data.kkn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiPengolahanDataKknApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiPengolahanDataKknApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpaydeveloper/sistem/informasi/pengolahan/data/kkn/SistemInformasiPengolahanDataKknApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpaydeveloper.sistem.informasi.pengolahan.data.kkn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiPengolahanDataKknApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
